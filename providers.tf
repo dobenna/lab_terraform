7fa8b6ea0df56d5449dc8425d6cf05e189859b71
dobenna <109390105+dobenna@users.noreply.github.com>
2023-01-30 21:55:49 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
     subscription_id = "2a2b4498-ca78-425f-a66d-faa2f24b07f4"
-    client_id = "7f397876-daa7-45b8-a234-8198d1fc3426"
-    client_secret = "0FH8Q~ip.O4YHnzwE65BHX.1pxn-cnUPxqZjmdu2"
-    tenant_id = "9dcc5461-a68b-4e41-8090-db19b856e3f2"
+    client_id = "--"
+    client_secret = "--"
+    tenant_id = "--"
     features {}
 }
 
